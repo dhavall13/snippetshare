12aaf553b2f42e3d2a335d8524266022e3800f82
dhvll <dhavalchaudhari39@gmail.com>
2024-09-16 23:50:21 +0530

    db connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -14,10 +16,19 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "http service address")
+	dsn := flag.String("dsn", "web:pass@/snippetshare?parseTime=true", "mysql data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// close connection pool before main func executes
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -29,8 +40,21 @@ func main() {
 		ErrorLog: errorLog,
 		Handler:  app.routes(),
 	}
+
 	// messages using new loggers
 	infoLog.Printf("Starting server on  %s", *addr)
-	err := srv.ListenAndServe()
+
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
