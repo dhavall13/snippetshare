02f8bbd65014cea35abe823ec739efc6279b136c
dhvll <dhavalchaudhari39@gmail.com>
2024-09-19 14:40:49 +0530

    custom template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,9 +20,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, http.StatusOK, "home.tmpl", &templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+	app.render(w, http.StatusOK, "home.tmpl", data)
 
 	//files := []string{
 	//	"./ui/html/base.tmpl",
@@ -59,29 +59,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
-	app.render(w, http.StatusOK, "view.tmpl", &templateData{
-		Snippet: snippet,
-	})
-
-	//files := []string{
-	//	"./ui/html/base.tmpl",
-	//	"./ui/html/partials/nav.tmpl",
-	//	"./ui/html/pages/view.tmpl",
-	//}
-	//
-	//ts, err := template.ParseFiles(files...)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//	return
-	//}
-	//
-	//data := &templateData{Snippet: snippet}
-	//
-	//err = ts.ExecuteTemplate(w, "base", data)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//}
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
