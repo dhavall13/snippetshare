d157568b68da651d2625b2923e901130f0ca26a5
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2024-09-28 03:38:45 +0530

    added few middlewares

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -13,7 +13,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.notFound(w)
 		return
 	}
-
+	//panic("testing")
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
